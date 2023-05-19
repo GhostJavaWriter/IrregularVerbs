9bcd8f3fde059d8af448ffe79f460bb68a2cd41d
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2023-05-19 10:52:09 -0700

    initial commit

---
+++ IrregularVerbs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IrregularVerbs
+//
+//  Created by Bair Nadtsalov on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+}
+
